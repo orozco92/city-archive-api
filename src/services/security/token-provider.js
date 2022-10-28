351ce18ff99b9f88f1c759d9909bce67dc98bd8e
orozco92 <goldstein.david92@gmail.com>
2022-10-28 10:13:15 -0400

    Update token to 7 days

--- src/services/security/token-provider.js
+++ src/services/security/token-provider.js
@@ -3,7 +3,7 @@ const _ = require('lodash');
 module.exports = (req, res, next) => {
     const user = _.pick(req.user, ['id', 'username', 'role', 'email', 'ci', 'name', 'lastName', 'email', 'phone', 'address', 'nationality'])
     const token = jwt.sign(user, req._config.secret, {
-        expiresIn: '1h'
+        expiresIn: '7d'
     })
     res.send({
         user: user,
