60f0859735b20d23643034aae487ecc01ee05d29
orozco92 <goldstein.david92@gmail.com>
2022-11-21 15:04:12 -0500

    Notarial protocols service request api Fixes #3

---
+++ src/routes/public.routes.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const router = express.Router();
+const queryParser = require('../services/query-parser');
+const modelService = require('../services/models');
+const publicModels = "(informative\-services|public\-funds)"
+
+router.get('/',
+    queryParser.order(),
+    // queryParser.include(),
+    queryParser.attributes(),
+    queryParser.where(),
+    queryParser.not(),
+    queryParser.search(),
+    // queryParser.model(),
+    queryParser.queryBuilder(),
+    async function (req, res, next) {
+        const query = req.Query
+        const model = req.Model
+        try {
+            const data = await modelService.getServiceForModel(model).list(query, {
+                scope: req.query.scope,
+                pagination: !req.query.noPaged
+            })
+            res.send(data)
+        } catch (e) {
+            next(e)
+        }
+    })
+
+router.get('/:id',
+    queryParser.order(),
+    // queryParser.include(),
+    queryParser.attributes(),
+    queryParser.queryBuilder(),
+    async function (req, res, next) {
+        const id = req.params.id
+        const query = req.Query
+        delete query.limit
+        delete query.offset
+        try {
+            const data = await modelService.getServiceForModel(req.Model).find(id, query, { scope: req.query.scope })
+            res.send(data)
+        } catch (e) {
+            next(e)
+        }
+    })
+
+module.exports = {
+    path: '/public/:model' + publicModels,
+    order: 2,
+    router,
+    middlewares: [queryParser.buildModel()]
+};
