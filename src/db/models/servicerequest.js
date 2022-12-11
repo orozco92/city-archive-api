371d1c38576b741f11aaf63987fad20e48b3790e
orozco92 <goldstein.david92@gmail.com>
2022-12-11 17:57:08 -0500

    Add user id to service request. Get request by user

--- src/db/models/servicerequest.js
+++ src/db/models/servicerequest.js
@@ -1,68 +1,72 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class ServiceRequest extends Model {
-
     static associate(models) {
       ServiceRequest.belongsTo(models.User, {
-        onDelete: 'CASCADE',
-        as: 'RequestedBy'
-      })
+        onDelete: "CASCADE",
+        as: "RequestedBy",
+      });
       ServiceRequest.belongsTo(models.InformativeService, {
-        onDelete: 'CASCADE'
-      })
+        onDelete: "CASCADE",
+      });
       ServiceRequest.hasOne(models.ServiceRequestForeignIndex, {
-        onDelete: 'CASCADE'
-      })
+        onDelete: "CASCADE",
+      });
       ServiceRequest.hasOne(models.ServiceRequestNotarialProtocol, {
-        onDelete: 'CASCADE'
-      })
+        onDelete: "CASCADE",
+      });
     }
 
     static getSearchAttributes() {
-      return ['ci', 'status']
+      return ["ci", "status"];
     }
-
   }
-  ServiceRequest.init({
-    ci: {
-      type: DataTypes.STRING,
-      validate: {
-        len: 11
-      }
+  ServiceRequest.init(
+    {
+      ci: {
+        type: DataTypes.STRING,
+        validate: {
+          len: 11,
+        },
+      },
+      name: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      email: {
+        type: DataTypes.STRING,
+        validate: {
+          isEmail: true,
+        },
+      },
+      address: DataTypes.TEXT,
+      nationality: DataTypes.STRING,
+      description: DataTypes.TEXT,
+      date: DataTypes.DATEONLY,
+      status: {
+        type: DataTypes.STRING,
+        defaultValue: "CREATED",
+      },
     },
-    name: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: {
-      type: DataTypes.STRING,
-      validate: {
-        isEmail: true
-      }
-    },
-    address: DataTypes.TEXT,
-    nationality: DataTypes.STRING,
-    date: DataTypes.DATEONLY,
-    status: {
-      type: DataTypes.STRING,
-      defaultValue: 'CREATED'
-    },
-  }, {
-    sequelize,
-    modelName: 'ServiceRequest',
-    tableName: 'service_requests',
-    underscored: true,
-    defaultScope: {
-      include: [{
-        association: 'InformativeService',
-        attributes: ['id', 'name']
-      }, {
-        association: 'ServiceRequestForeignIndex'
-      }, {
-        association: 'ServiceRequestNotarialProtocol'
-      }]
+    {
+      sequelize,
+      modelName: "ServiceRequest",
+      tableName: "service_requests",
+      underscored: true,
+      defaultScope: {
+        include: [
+          {
+            association: "InformativeService",
+            attributes: ["id", "name"],
+          },
+          {
+            association: "ServiceRequestForeignIndex",
+          },
+          {
+            association: "ServiceRequestNotarialProtocol",
+          },
+        ],
+      },
     }
-  });
+  );
   return ServiceRequest;
 };
--- src/services/models/service-request.model.js
+++ src/services/models/service-request.model.js
@@ -1,15 +1,15 @@
 const DefaultModelService = require("./default-model-service");
 
 class ServiceRequestModel extends DefaultModelService {
-    static name = 'ServiceRequest'
+  static name = "ServiceRequest";
 
-    async save(body, id, opts) {
-        if (!id || !body.date) {
-            body.date = new Date();
-        }
-        body.UserId = opts.user
-        return super.save(body, id, opts);
+  async save(body, id, opts) {
+    if (!id || !body.date) {
+      body.date = new Date();
     }
+    body.RequestedById = opts.user.id;
+    return super.save(body, id, opts);
+  }
 }
 
 module.exports = ServiceRequestModel;
